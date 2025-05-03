c0b1c1fe660be332ab2b0149d56d910d49761b94
William-Mata <william.xavante@gmail.com>
2025-05-03 19:40:28 -0300

    Funcionalidade de Alternar Entre Idiomas

--- Financeiro/financeiro-front/src/app/layout/sidebar/sidebar.component.ts
+++ Financeiro/financeiro-front/src/app/layout/sidebar/sidebar.component.ts
@@ -1,7 +1,9 @@
+import { TraducaoService } from './../../core/services/translate/traducao.service';
 import { Component } from '@angular/core';
 import { ImportacaoPadrao } from '../../shared/imports/importacao.padrao.shared';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-sidebar',
   imports:[
@@ -15,6 +17,11 @@ import { MatIconModule } from '@angular/material/icon';
 
 export class SidebarComponent {
   isExpandido = false;
+  idiomaAtual : string = "pt";
+
+  constructor(private traducaoService: TraducaoService) {
+    this.idiomaAtual = this.traducaoService.buscarIdiomaAtual();
+  }
 
   alternarSidebar() {
     document.getElementById('wrapper')?.classList.toggle('toggled');
@@ -25,5 +32,10 @@ export class SidebarComponent {
     const body = document.body;
     body.classList.toggle('light-theme');
     body.classList.toggle('dark-theme');
+  }
+
+  async alterarIdioma(idioma : string) {
+    this.idiomaAtual = idioma ?? this.idiomaAtual;
+    this.traducaoService.alterarIdioma(this.idiomaAtual);
   }
 }
